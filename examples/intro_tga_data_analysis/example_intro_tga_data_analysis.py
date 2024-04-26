d482346bf54138e4caace1d5f5b2bbf053a8daf9
mpecchi <matteo.pecchi@gmail.com>
2024-04-26 15:47:33 -0400

    added comments to examples

--- examples/intro_tga_data_analysis/example_intro_tga_data_analysis.py
+++ examples/intro_tga_data_analysis/example_intro_tga_data_analysis.py
@@ -2,32 +2,42 @@
 import pathlib as plib
 from tga_data_analysis.tga import Project, Sample
 
-test_dir = plib.Path(__file__).resolve().parent
-
-
-# %% test different units
+# this is the relative path to the folder where the data is
+# which is the same as where the script is __file__
+folder_path = plib.Path(__file__).resolve().parent
+# if running as a Jupyter notebook, use absolute paths
+# folder_path = plib.Path("absolute path to folder")
+# %%
+# create the project instance, important parameters are:
+# the moisture value is used to compute the dry basis curve
 proj_default = Project(
-    test_dir,
-    name="default",
-    temp_unit="C",
-    plot_font="Dejavu Sans",
-    dtg_basis="temperature",
-    resolution_sec_deg_dtg=5,
-    dtg_window_filter=101,
-    plot_grid=False,
-    temp_initial_celsius=40,
-    temp_lim_dtg_celsius=None,
+    folder_path,
+    name="default",  # the name of the project
+    temp_unit="C",  # the temperature that results will use (C or K)
+    plot_font="Dejavu Sans",  # chose the font for the plots
+    dtg_basis="temperature",  # chose the DTG units (wt%/temp or /time)
+    resolution_sec_deg_dtg=5,  # chose the resolution for dtg vectors
+    dtg_window_filter=101,  # chose the filtering window for dtg curve
+    plot_grid=False,  # wheter to include a grid in plots
+    temp_initial_celsius=40,  # initial temperature for all curves (exclude data before)
+    temp_lim_dtg_celsius=None,  # temperature limits for the dtg curves
+    time_moist=38,  # the time where mass loss due to moisture is computed,
+    time_vm=None,  # specify if there is a step to evaluate volatile matter
 )
+# add the first sample
 cell = Sample(
-    project=proj_default,
-    name="cell",
-    filenames=["CLSOx5_1", "CLSOx5_2", "CLSOx5_3"],
-    time_moist=38,
-    time_vm=None,
+    project=proj_default,  # specify what project it belongs to
+    name="cell",  # sample name
+    filenames=["CLSOx5_1", "CLSOx5_2", "CLSOx5_3"],  # filenames in the project folder
+    time_moist=38,  # this was already specified in the project, but it can be overwritten
+    time_vm=None,  # same as time_moist
 )
+# plot the tg_dtg plot for the cell sample (show all replicates to allow checks)
 mf = cell.plot_tg_dtg()
+# %%
+# specify a difference project instance with different parameters
 proj_mod = Project(
-    test_dir,
+    folder_path,
     name="mod",
     temp_unit="K",
     plot_font="Times New Roman",
@@ -37,14 +47,16 @@ proj_mod = Project(
     plot_grid=True,
     temp_initial_celsius=50,
     temp_lim_dtg_celsius=(150, 750),
+    time_moist=38,
+    time_vm=None,
 )
+# add a sample to the new project instance
 cell = Sample(
     project=proj_mod,
     name="cell",
     filenames=["CLSOx5_1", "CLSOx5_2", "CLSOx5_3"],
-    time_moist=38,
-    time_vm=None,
 )
+# plot the tg_dtg plot for the cell sample (show all replicates to allow checks)
 mf = cell.plot_tg_dtg()
 
 
--- examples/proximate_analysis/example_proximate.py
+++ examples/proximate_analysis/example_proximate.py
@@ -1,26 +1,52 @@
+# %%
 import pathlib as plib
 from tga_data_analysis.tga import Project, Sample
 
-test_dir = plib.Path(__file__).resolve().parent
-
+# this is the relative path to the folder where the data is
+# which is the same as where the script is __file__
+folder_path = plib.Path(__file__).resolve().parent
+# if running as a Jupyter notebook, use absolute paths
+# folder_path = plib.Path("absolute path to folder")
 # %%
-proj = Project(test_dir, name="test", temp_unit="K")
+# create the project with default paramters for all samples
+proj = Project(
+    folder_path,
+    temp_unit="K",  # the temperature used in the results (not in the inputs)
+    time_moist=38,  # the time (min) where the moisture content shoudl be taken
+    time_vm=167,  # the time (min) where the volatile matter should be taken
+    dtg_basis="temperature",  # either "time" or "temperature" (should be temperature)
+)
+# add the first sample, giving names and filenames (txt files in the folder path)
 sru = Sample(
-    project=proj, name="sru", filenames=["SRU_1", "SRU_2", "SRU_3"], time_moist=38, time_vm=167
+    project=proj,
+    name="sru",
+    filenames=["SRU_1", "SRU_2", "SRU_3"],
 )
+# add the second file (some parameters are overwritten)
 misc = Sample(
-    project=proj, name="misc", filenames=["MIS_1", "MIS_2", "MIS_3"], time_moist=38, time_vm=147
+    project=proj,
+    name="misc",
+    filenames=["MIS_1", "MIS_2", "MIS_3"],
+    time_vm=147,  # change the value for this sample only
 )
-
+# for each sample, create tg_dtg plots to visually check results
 proximate_samples = [sru, misc]
 for sample in proximate_samples:
     _ = sample.plot_tg_dtg()
+# create a df report (rep) and export it in excel
 rep = proj.multireport(
-    samples=proximate_samples,
-    report_type="proximate",
+    samples=proximate_samples,  # samples to include
+    report_type="proximate",  # specify the type of report
+    report_style="ave_pm_std",  # specify the style of the report
 )
-rep = proj.plot_multireport(
-    "rep_",
+
+# PLOT CUSTOMIZATION
+# plots can be customized using the kwargs in each plotting method
+# the list of valid kwargs is in the MyFigure documentation (or code)
+
+# plot the results of the report as a barplot
+mf = proj.plot_multireport(
+    "proximate_plot",
     samples=proximate_samples,
     report_type="proximate",
     height=4,
@@ -28,12 +54,17 @@ rep = proj.plot_multireport(
     y_lim=(0, 100),
     # legend_loc="upper left",
 )
-# %%
-rep = proj.plot_multi_dtg(
-    "rep",
+# plot the TG curves for each sample as ave+-std
+mf = proj.plot_multi_tg(
+    "tg_plot",
     samples=proximate_samples,
     height=4,
     width=4.5,
 )
-
-# %%
+# plot the DTG curves for each sample as ave+-std
+mf = proj.plot_multi_dtg(
+    "dtg_plot",
+    samples=proximate_samples,
+    height=4,
+    width=4.5,
+)
